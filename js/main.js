5251dcb43b2b57c51c38ae6a9ba77e9a0c1609a1
footzi <footzi@mail.ru>
2017-10-03 19:09:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.farm__images_carousel').bxSlider({
+        auto: true,
+        slideWidth: 300,
+        minSlides: 4,
+        maxSlides: 4,
+        slideMargin: 50
+    });
+  });
