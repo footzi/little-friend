205d9337e80ff0e25cc5689bea8814e7f3c12c08
footzi <footzi@mail.ru>
2017-10-19 07:08:45 +0300

    ajax add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-    $('.farm__images_carousel').bxSlider({
-        auto: true,
-        slideWidth: 300,
-        minSlides: 4,
-        maxSlides: 4,
-        slideMargin: 50
-    });
-  });
+    if($(".farm__images_carousel").length) {
+        $('.farm__images_carousel').bxSlider({
+            auto: true,
+            slideWidth: 300,
+            minSlides: 4,
+            maxSlides: 4,
+            slideMargin: 50
+        });
+    };
+
+    $('.header').load('components/header.html');
+    $('.nav').load('components/nav.html');
+    if($(".issues").length) {
+        $('.issues').load('components/issues.html');
+    }
+    $('.footer').load('components/footer.html');
+});
